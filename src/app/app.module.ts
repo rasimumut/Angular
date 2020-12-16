ad4cfe5c860a631353046efb711d7dea434f8109
rasimumut <rsmumtozkrt5@gmail.com>
2020-12-16 20:04:25 +0300

    addcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MoviesComponent} from "./movies/movies.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "movies",
+  templateUrl: "movies.compenent.html"
+})
+
+export class MoviesComponent { }
